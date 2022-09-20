a22b8be7053271efcef7a6d946d30ec75477cef6
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2022-09-21 00:00:14 +0400

    Update animation

--- logoanimation.py
+++ logoanimation.py
@@ -2,36 +2,34 @@ from manim import *
 
 class Testing(Scene):
     def construct(self):
-        # square = Square()
-        # square.width = 3
+        speed = 0.6
+        c_color = '#F78D3F'
+        sharp_color = '#ffffff'
 
-        START = (-2,0,0)
-        END =   (2,0,0)
-        # line1 = Line((-2,0,0),(2,0,0))
-        line2 = Line((-2,1,0),(2,1,0), stroke_width = 20)
-        # line3 = Line((0,2,0), (0,-2,0))
-        line4 = Line((1,-2,0),(1,2,0), stroke_width = 20)
-        # line2.next_to(line1, UP, buff=1)
-        # line4.next_to(line3, LEFT, buff=1)
+        line1 = Line((-2,1,0),(2,1,0), stroke_width = 20, color = sharp_color)
+        line2 = Line((1,-2,0),(1,2,0), stroke_width = 20, color = sharp_color)
 
-        self.play(Create(line2))
-        self.play(Create(line4))
-        line2.add(line4)
+        self.play(Create(line1),Create(line2), run_time=speed)
+        line1.add(line2)
 
         # Different inner_radius and outer_radius than the default.
-        s2 = AnnularSector(inner_radius=1.5, outer_radius=2, angle=270 * DEGREES, color=RED).rotate(45 * DEGREES)
+        s2 = AnnularSector(inner_radius=1.5, outer_radius=2, angle=270 * DEGREES, color=sharp_color).rotate(45 * DEGREES)
 
         # some animations display mobjects, ...
-        self.play(FadeIn(s2))
+        self.play(Create(s2), run_time=speed)
 
         # some animations remove mobjects from the screen
-        self.play(Transform(s2, line2))
-
-        # ... some move or rotate mobjects around...
-        self.play(Rotate(s2, PI))
-        s3 = AnnularSector(inner_radius=1.5, outer_radius=2, angle=270 * DEGREES, color=RED).rotate(45 * DEGREES)
-        self.play(FadeIn(s3))
-        s2.add(line2)
-        self.play(s2.animate.scale(0.5))
-        self.play(s2.animate.shift(RIGHT)),
+        line3 = Line((-2,1,0),(2,1,0), stroke_width = 20, color = sharp_color)
+        line4 = Line((1,-2,0),(1,2,0), stroke_width = 20, color = sharp_color)
+        line3.add(line4).rotate(PI)
+        self.play(Transform(s2, line3), run_time=speed)
+        
+        s3 = AnnularSector(inner_radius=1.5, outer_radius=2, angle=270 * DEGREES, color=c_color).rotate(45 * DEGREES)
+        self.play(Create(s3), run_time=1)
+        
+        s2.add(line1)
+        
+        self.play(s2.animate.scale(0.5), run_time=speed)
+        self.play(s2.animate.shift(RIGHT), run_time=speed),
+        
         self.wait(1)
