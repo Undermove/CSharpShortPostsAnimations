4c53cb634905840fe49954088a00e8dd7b01ef94
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2022-09-25 01:07:11 +0400

    Add C# sticker

---
+++ stickerpak/csharpSticker.py
@@ -0,0 +1,26 @@
+from manim import *
+
+class CsharpSticker(Scene):
+    def construct(self):
+        speed = 0.3
+        c_color = '#F78D3F'
+        sharp_color = '#ffffff'
+
+        text = MarkupText("<span size=\"x-large\">Це</span>")
+        self.play(Create(text), run_time=speed)
+        self.wait(1)
+
+        s2 = AnnularSector(inner_radius=1.5, outer_radius=2, angle=270 * DEGREES, color=c_color).rotate(45 * DEGREES)
+        self.play(Transform(text, s2), run_time=speed)
+
+        text2 = MarkupText("<span style=\"italic\" size=\"x-large\">РЕШЁТКА!</span>")
+        self.play(Create(text2), run_time=speed)
+        self.wait(1)
+
+        line1 = Line((-2,1,0),(2,1,0), stroke_width = 20, color = sharp_color)
+        line2 = Line((1,-2,0),(1,2,0), stroke_width = 20, color = sharp_color)
+        line3 = Line((-2,-1,0),(2,-1,0), stroke_width = 20, color = sharp_color)
+        line4 = Line((-1,-2,0),(-1,2,0), stroke_width = 20, color = sharp_color)
+        line1.add(line2).add(line3).add(line4)
+        self.play(Transform(text2,line1), run_time=speed)
+        self.wait(1)
