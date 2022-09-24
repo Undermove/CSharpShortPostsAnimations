b58bca8ec7b0ecf42364f578d6dfdc46cb691e43
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2022-09-25 02:04:57 +0400

    Remove redundant color

--- stickerpak/tochkaNet.py
+++ stickerpak/tochkaNet.py
@@ -4,7 +4,6 @@ class TochkaNet(Scene):
     def construct(self):
         speed = 0.3
         c_color = '#FF813F'
-        sharp_color = '#ffffff'
 
         text = MarkupText("<span size=\"x-large\">ТОЧКА!</span>")
         self.play(Create(text), run_time=speed)
@@ -14,7 +13,7 @@ class TochkaNet(Scene):
         self.play(Transform(text, circle), run_time=speed)
         self.wait(1)
         
-        circle2 = Circle(radius=1.3, color=c_color, fill_opacity=1)
+        circle2 = Circle(radius=1.4, color=c_color, fill_opacity=1)
         self.play(Transform(text, circle2), run_time=speed)
         self.play(Create(circle2.next_to(circle)), run_time=speed)
         self.play(Create(circle2.next_to(circle, direction=LEFT)), run_time=speed)
@@ -24,4 +23,8 @@ class TochkaNet(Scene):
 
         text2 = MarkupText("<span style=\"italic\" size=\"x-large\">НЭТ!</span>")
         self.play(Transform(circle2, text2), run_time=speed)
+        self.wait(1)
+
+        text3 = MarkupText("<span style=\"italic\" size=\"x-large\">.NET</span>")
+        self.play(Transform(circle2, text3), run_time=speed)
         self.wait(1)
