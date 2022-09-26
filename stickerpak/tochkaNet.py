576c66f2f8e50841c5f5266eee596110d5d5f56d
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2022-09-26 15:05:51 +0400

    little changes for dot net sticker

--- stickerpak/tochkaNet.py
+++ stickerpak/tochkaNet.py
@@ -5,7 +5,7 @@ class TochkaNet(Scene):
         speed = 0.3
         c_color = '#FF813F'
 
-        text = MarkupText("<span size=\"x-large\">ТОЧКА!</span>")
+        text = MarkupText("<span size=\"x-large\">НЭТ!</span>")
         self.play(Create(text), run_time=speed)
         self.wait(1)
 
@@ -21,7 +21,7 @@ class TochkaNet(Scene):
         self.play(Create(circle2.next_to(circle, direction=DOWN*1)), run_time=speed)
         self.wait(1)
 
-        text2 = MarkupText("<span style=\"italic\" size=\"x-large\">НЭТ!</span>")
+        text2 = MarkupText("<span style=\"italic\" size=\"x-large\">И ТОЧКА!</span>")
         self.play(Transform(circle2, text2), run_time=speed)
         self.wait(1)
 
