30bf3928b819af46fb7978e16ac456e3167b5760
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2022-09-30 10:41:58 +0400

    Finish fuck animation

--- stickerpak/fuck.py
+++ stickerpak/fuck.py
@@ -2,37 +2,40 @@ from manim import *
 
 class Fuck(Scene):
     def construct(self):
-        speed = 0.3
+        speed = 0.2
         f_color = '#FF0000'
         sharp_color = '#00FF00'
         c_color = '#0000FF'
-        k_color = '#00FFFF'
+        k_color = '#F1F114'
 
-        line1 = Line((-1,1,0),(0.75,1,0), stroke_width = 20, color = f_color)
-        line2 = Line((-1,0.1,0),(0.5,0.1,0), stroke_width = 20, color = f_color)
-        line3 = Line((-1,-1,0),(-1,1,0), stroke_width = 20, color = f_color)
-        line1.add(line2).add(line3)
-        line1.scale(2)
-        self.play(Create(line1), run_time=speed)
+        f = Line((-1.08,1,0),(0.75,1,0), stroke_width = 20, color = f_color)
+        f_line2 = Line((-1,0.1,0),(0.5,0.1,0), stroke_width = 20, color = f_color)
+        f_line3 = Line((-1,-1,0),(-1,1,0), stroke_width = 20, color = f_color)
+        f.add(f_line2).add(f_line3)
+        f.scale(2)
+        self.play(Create(f), run_time=speed)
 
-        line4 = Line((-2,1,0),(2,1,0), stroke_width = 20, color = sharp_color)
-        line5 = Line((1,-2,0),(1,2,0), stroke_width = 20, color = sharp_color)
-        line6 = Line((-2,-1,0),(2,-1,0), stroke_width = 20, color = sharp_color)
-        line7 = Line((-1,-2,0),(-1,2,0), stroke_width = 20, color = sharp_color)
-        line4.add(line5).add(line6).add(line7)
-        self.play(Create(line4, run_time=speed))
-        # self.play(line1.animate.shift(1.5*LEFT), line4.animate.shift(1.5*RIGHT), run_time=speed)
+        sharp = Line((-2,1,0),(2,1,0), stroke_width = 20, color = sharp_color)
+        sharp_line5 = Line((1,-2,0),(1,2,0), stroke_width = 20, color = sharp_color)
+        sharp_line6 = Line((-2,-1,0),(2,-1,0), stroke_width = 20, color = sharp_color)
+        sharp_line7 = Line((-1,-2,0),(-1,2,0), stroke_width = 20, color = sharp_color)
+        sharp.add(sharp_line5).add(sharp_line6).add(sharp_line7)
+        self.play(Create(sharp, run_time=speed))
 
-        line4_copy = line4.copy()
+        c = AnnularSector(inner_radius=1.5, outer_radius=2, angle=270 * DEGREES, color=c_color).rotate(45 * DEGREES)
+        self.play(Create(c), run_time=speed)
 
-        s2 = AnnularSector(inner_radius=1.5, outer_radius=2, angle=270 * DEGREES, color=c_color).rotate(45 * DEGREES)
-        self.play(Transform(line4_copy, s2), run_time=speed)
-        self.wait(1)
-
-        k_line1 = Line((-1,0,0),(0.75,1,0), stroke_width = 20, color = k_color)
+        k = Line((-1,0,0),(0.75,1,0), stroke_width = 20, color = k_color)
         k_line2 = Line((-0.75,0.18,0),(0.75,-1,0), stroke_width = 20, color = k_color)
         k_line3 = Line((-1,-1,0),(-1,1,0), stroke_width = 20, color = k_color)
-        k_line1.add(k_line2).add(k_line3)
-        k_line1.scale(2)
-        self.play(Create(k_line1, run_time=speed))
+        k.add(k_line2).add(k_line3)
+        k.scale(2)
+
+        self.play(f.animate.scale(0.5), sharp.animate.scale(0.5), c.animate.scale(0.5))
+        self.play(f.animate.shift(3*LEFT), sharp.animate.shift(1.2*LEFT), c.animate.shift(1.2*RIGHT))
+
+        self.wait(0.2)
+        k.scale(0.5)
+        k.shift(3*RIGHT)
+        self.play(Create(k, run_time=speed))
         self.wait(1)
