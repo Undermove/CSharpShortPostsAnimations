554d0ac6f1ff4c0303e6654af58368bbcdcedf74
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2022-10-13 10:59:51 +0400

    finish pozhaluista

--- stickerpak/pozhaluista.py
+++ stickerpak/pozhaluista.py
@@ -19,12 +19,14 @@ class Pozhaluista(Scene):
             run_time=speed)
         self.wait(0.5)
 
-        text_async = MarkupText(f'<span color=\"{blue_color}\" size=\"large\">{{</span>')
-        text_spasibo = MarkupText(f'<span color=\"{orange_color}\" size=\"large\">await Pozhaluista();</span>')
+        text_leftbrace = MarkupText(f'<span color=\"{blue_color}\" size=\"large\">{{</span>')
+        text_await = MarkupText(f'<span color=\"{blue_color}\" size=\"large\">await</span>')
+        text_pozhaluista = MarkupText(f'<span color=\"{orange_color}\" size=\"large\">Pozhaluista();</span>')
         text_parentheses = MarkupText(f'<span color=\"{blue_color}\" size=\"large\">}}</span>')
         self.play(
-            Transform(point1, text_async.shift(1*UP).shift(3*LEFT)),
-            Transform(point2, text_spasibo.next_to(text_async, direction=DOWN).shift(4*RIGHT)), 
-            Transform(point3, text_parentheses.next_to(text_spasibo, direction=DOWN).shift(4*LEFT)),
+            Transform(point1, text_leftbrace.shift(1*UP).shift(3*LEFT)),
+            Transform(point2, text_await.next_to(text_leftbrace, direction=DOWN).shift(2*RIGHT)), 
+            Create(text_pozhaluista.next_to(text_await).shift(0.05*DOWN)), 
+            Transform(point3, text_parentheses.next_to(text_await, direction=DOWN).shift(2*LEFT)),
             run_time=speed)
         self.wait(2)
