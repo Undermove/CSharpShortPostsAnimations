3c0ffe0534dabdc7890b952f61f0399037efb647
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2022-10-14 12:32:49 +0400

    finish krut'

--- stickerpak/krut.py
+++ stickerpak/krut.py
@@ -4,15 +4,15 @@ class Krut(Scene):
     def construct(self):
         self.camera.background_color = "#FFFFFF00"
         speed = 0.3
-        c_color = '#F78D3F'
-        sharp_color = '#500000'
+        c_color = '#ff0000'
+        sharp_color = '#ff5a00'
+
+        text2 = MarkupText(f'<span color=\"{sharp_color}\" style=\"italic\" size=\"x-large\">КРУТЬ!</span>')
+        self.play(Create(text2.shift(2*RIGHT)), run_time=speed)
+        self.wait(1)
 
         s2 = AnnularSector(inner_radius=1.5, outer_radius=2, angle=270 * DEGREES, color=c_color).rotate(45 * DEGREES)
         self.play(Create(s2), run_time=speed)
-
-        text2 = MarkupText(f'<span color=\"{sharp_color}\" style=\"italic\" size=\"xx-large\">КРУТЬ!</span>')
-        self.play(Create(text2.shift(2*RIGHT)), run_time=speed)
-        self.wait(1)
 
         s2.add(text2)
 
@@ -26,6 +26,6 @@ class Krut(Scene):
                 s2,
                 angle=4.5*PI,
                 about_point=ORIGIN,
-                rate_func=linear,
+                rate_func=rate_functions.ease_in_out_sine,
             ), run_time=speed)
         self.wait(1)
