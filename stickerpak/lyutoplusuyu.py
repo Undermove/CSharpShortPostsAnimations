9aba15e102fea3d01aa948fb3de08039886411bc
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-09-06 23:27:36 +0400

    Fix luytoplusuyu

--- stickerpak/lyutoplusuyu.py
+++ stickerpak/lyutoplusuyu.py
@@ -7,8 +7,8 @@ class LyutoPlusuyu(Scene):
         c_color = '#BBFF33'
         sharp_color = '#00FF00'
 
-        line1 = Line((-1,0,0),(1,0,0), stroke_width = 20, color = sharp_color)
-        line2 = Line((0,-1,0),(0,1,0), stroke_width = 20, color = sharp_color)
+        line1 = Line((-3,0,0),(3,0,0), stroke_width = 30, color = sharp_color)
+        line2 = Line((0,-3,0),(0,3,0), stroke_width = 30, color = sharp_color)
         line1.add(line2)
         line3 = line1.copy()
         self.play(Create(line1), run_time=speed)
@@ -23,15 +23,15 @@ class LyutoPlusuyu(Scene):
         
         line4.add(line1)
         self.play(line4.animate.scale(0.5), run_time=speed)
-        self.play(line4.animate.shift(UP*1.1), run_time=speed)
+        self.play(line4.animate.shift(UP*3.1), run_time=speed)
 
-        s = AnnularSector(inner_radius=1.5, outer_radius=2, angle=270 * DEGREES, color=c_color).rotate(45 * DEGREES)
+        s = AnnularSector(inner_radius=3.5, outer_radius=4.5, angle=270 * DEGREES, color=c_color).rotate(45 * DEGREES)
         s.generate_target()
         s.target.move_to(1.5*LEFT)
         # self.play(Create(s), run_time=speed)
 
         self.play(Create(s), MoveToTarget(s), run_time=speed)
     
-        text2 = MarkupText(f'<span color=\"{sharp_color}\" style=\"italic\" size=\"large\">ЛЮТО ПЛЮСУЮ!</span>')
-        self.play(Create(text2.next_to(line4, direction=5*DOWN)), run_time=speed)
+        text2 = MarkupText(f'<span color=\"{sharp_color}\" style=\"italic\" size=\"25pt\">ЛЮТО ПЛЮСУЮ!</span>')
+        self.play(Create(text2.next_to(line4, direction=7*DOWN).shift(2.2*LEFT)), run_time=speed)
         self.wait(1)
