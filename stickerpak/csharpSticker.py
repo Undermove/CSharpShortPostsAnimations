fbcc0c5660461ef8556e14734e4c9e87c22c963e
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-09-09 22:46:10 +0400

    Fix csharp logo

--- stickerpak/csharpSticker.py
+++ stickerpak/csharpSticker.py
@@ -7,21 +7,21 @@ class CsharpSticker(Scene):
         c_color = '#F78D3F'
         sharp_color = '#500000'
 
-        text = MarkupText(f'<span color=\"{c_color}\" size=\"xx-large\">Цэ</span>')
+        text = MarkupText(f'<span color=\"{c_color}\" size=\"30pt\">ЦЭ</span>')
         self.play(Create(text), run_time=speed)
-        self.wait(1)
+        self.wait(0.3)
 
-        s2 = AnnularSector(inner_radius=1.5, outer_radius=2, angle=270 * DEGREES, color=c_color).rotate(45 * DEGREES)
+        s2 = AnnularSector(inner_radius=3.5, outer_radius=4.5, angle=270 * DEGREES, color=c_color).rotate(45 * DEGREES)
         self.play(Transform(text, s2), run_time=speed)
 
-        text2 = MarkupText(f'<span color=\"{sharp_color}\" style=\"italic\" size=\"large\">РЕШЁТКА!</span>')
+        text2 = MarkupText(f'<span color=\"{sharp_color}\" style=\"italic\" size=\"30pt\">РЕШЁТКА!</span>')
         self.play(Create(text2), run_time=speed)
-        self.wait(1)
+        self.wait(0.3)
 
-        line1 = Line((-2,1,0),(2,1,0), stroke_width = 20, color = sharp_color)
-        line2 = Line((1,-2,0),(1,2,0), stroke_width = 20, color = sharp_color)
-        line3 = Line((-2,-1,0),(2,-1,0), stroke_width = 20, color = sharp_color)
-        line4 = Line((-1,-2,0),(-1,2,0), stroke_width = 20, color = sharp_color)
+        line1 = Line((-4,2,0),(4,2,0), stroke_width = 40, color = sharp_color)
+        line2 = Line((2,-4,0),(2,4,0), stroke_width = 40, color = sharp_color)
+        line3 = Line((-4,-2,0),(4,-2,0), stroke_width = 40, color = sharp_color)
+        line4 = Line((-2,-4,0),(-2,4,0), stroke_width = 40, color = sharp_color)
         line1.add(line2).add(line3).add(line4)
         self.play(Transform(text2,line1), run_time=speed)
-        self.wait(1)
+        self.wait(0.5)
