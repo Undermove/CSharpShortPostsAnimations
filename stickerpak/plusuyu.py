8a7424730916863e72d8bf98248737c02329c618
Dmitry Afonchenko <d.afonchenko@dodopizza.com>
2023-09-09 23:09:33 +0400

    Fix plusuyu

--- stickerpak/plusuyu.py
+++ stickerpak/plusuyu.py
@@ -7,8 +7,8 @@ class Plusuyu(Scene):
         c_color = '#FF81FF'
         sharp_color = '#f000ff'
 
-        line1 = Line((-1,0,0),(1,0,0), stroke_width = 20, color = sharp_color)
-        line2 = Line((0,-1,0),(0,1,0), stroke_width = 20, color = sharp_color)
+        line1 = Line((-2,0,0),(2,0,0), stroke_width = 40, color = sharp_color)
+        line2 = Line((0,-2,0),(0,2,0), stroke_width = 40, color = sharp_color)
         line1.add(line2)
         line3 = line1.copy()
         self.play(Create(line1), run_time=speed)
@@ -17,14 +17,14 @@ class Plusuyu(Scene):
         line1.target.move_to(LEFT)
         self.play(MoveToTarget(line1), run_time=speed)
         self.play(Create(line3.next_to(line1)), run_time=speed)
-        self.wait(1)
+        self.wait(0.3)
 
-        s = AnnularSector(inner_radius=1.5, outer_radius=2, angle=270 * DEGREES, color=c_color).rotate(45 * DEGREES)
+        s = AnnularSector(inner_radius=3.5, outer_radius=4.5, angle=270 * DEGREES, color=c_color).rotate(45 * DEGREES)
         s.generate_target()
         self.play(Create(s), run_time=speed)
         s.target.move_to(1.5*LEFT)
         self.play(MoveToTarget(s), run_time=speed)
     
-        text2 = MarkupText(f'<span color=\"{sharp_color}\" style=\"italic\" size=\"x-large\">ПЛЮСУЮ!</span>')
-        self.play(Create(text2.next_to(line3, direction=5*DOWN)), run_time=speed)
-        self.wait(1)
+        text2 = MarkupText(f'<span color=\"{sharp_color}\" style=\"italic\" size=\"25pt\">ПЛЮСУЮ!</span>')
+        self.play(Create(text2.next_to(line3, direction=11*DOWN).shift(2.2*LEFT)), run_time=speed)
+        self.wait(0.5)
